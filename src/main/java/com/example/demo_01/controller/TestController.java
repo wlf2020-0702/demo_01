7ede03d57b58c29e5431dab7fa52669c9d529679
wlf <lifa.wang@thizgroup.com>
2020-07-02 17:59:21 +0800

    Initial commit

---
+++ src/main/java/com/example/demo_01/controller/TestController.java
@@ -0,0 +1,22 @@
+package com.example.demo_01.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Auther: wlf
+ * @Date: 2020/7/2
+ * @Description:
+ */
+@RestController
+@RequestMapping("/demo")
+public class TestController {
+
+  @GetMapping(value = "/test")
+  @ResponseBody
+  public String test() {
+    return "true";
+  }
+}
